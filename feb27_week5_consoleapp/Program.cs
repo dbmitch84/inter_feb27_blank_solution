27a52c865f6d35c9edf0daa994e51b0aff475b9b
Mitchell Wommack <Dwmitch84@gmail.com>
2020-02-27 10:09:52 -0800

    added Console App

---
+++ feb27_week5_consoleapp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace feb27_week5_consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
