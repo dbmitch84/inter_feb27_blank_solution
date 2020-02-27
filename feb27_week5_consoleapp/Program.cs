d3ab19728d687a07a11563a7d56f1089acd36e62
Mitchell Wommack <Dwmitch84@gmail.com>
2020-02-27 10:28:42 -0800

    added more stuff!

--- feb27_week5_consoleapp/Program.cs
+++ feb27_week5_consoleapp/Program.cs
@@ -10,19 +10,40 @@ namespace feb27_week5_consoleapp
     {
         static void Main(string[] args)
         {
+            string name = "Mitch";
             SayHello();
             SayGoodbye();
+            SayHelloTo(name);
+            SayGoodbyeTo(name);
         }
 
         public static void SayHello()
         {
             Console.WriteLine("Hello!!!");
+            Console.WriteLine();// blank line for spacing
         }
 
 
         public static void SayGoodbye()
         {
             Console.WriteLine("Ciao (That's fancy for 'goodbye')!!!");
+            Console.WriteLine();// blank line for spacing
         }
+
+        public static void SayHelloTo(string name)
+        {
+            
+            Console.WriteLine("Hello {0}!!!",name);
+            Console.WriteLine();// blank line for spacing
+        }
+
+
+        public static void SayGoodbyeTo(string name)
+        {
+            
+            Console.WriteLine("Ciao (That's fancy for 'goodbye') {0}!!!", name);
+            Console.WriteLine();// blank line for spacing
+        }
+
     }
 }
